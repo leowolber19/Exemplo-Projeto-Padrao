5c15d3064b984d109535c9cc85e14be188d7495c
Leonardo Wolber <leonardo19_wolber@hotmail.com>
2023-09-13 11:24:37 -0300

    datasource

--- views/produto.js
+++ views/produto.js
@@ -7,57 +7,16 @@ function produtoView(){
     exibeConteudoModulo(view);
 
    var grid = new gridjs.Grid({
-        columns: ['Nome', 'E-mail', 'Telefone'],
         search: true,
         sort: true,
         pagination: true,
         fixedHeader: true,
         resizable: true,
-        columns: [{
-          id: 'name',
-          name: 'Name'
-        }, {
-            id: 'email',
-            name: 'Email'
-        }, {
-            id: 'phoneNumber',
-            name: 'Phone Number'
-        }],
-        data: [
-          { name: 'John', email: 'john@example.com', phoneNumber: '(353) 01 222 3333' },
-          { name: 'Mark', email: 'mark@gmail.com', phoneNumber: '(01) 22 888 4444' },
-        ],
-        // data: () => {
-        //   return new Promise(resolve => {
-        //     setTimeout(() =>
-        //       resolve([
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //         ['John', 'john@example.com', '(353) 01 222 3333'],
-        //         ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
-        //         ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
-        //         ['Nisen', 'nis900@gmail.com',   '313 333 1923'],
-        //       ]), 500);
-        //   });
-        // },
+        columns: ['name', 'model', 'consumables'],
+        server: {
+          url: 'https://swapi.dev/api/vehicles',
+          then: data => data.results.map(movie => [movie.name, movie.model, movie.consumables])
+        }, 
         language: traducoes
       }).render(document.getElementById("produtoGrid"));
 
